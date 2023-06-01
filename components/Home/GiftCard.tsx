a6b5180570e02112a0a31559b26a2954486f097d
Anibe <samanibe@yahoo.com>
2023-06-01 16:29:05 +0100

    added components

---
+++ components/Home/GiftCard.tsx
@@ -0,0 +1,359 @@
+"use client";
+import { Fragment, useState } from 'react'
+import { Dialog, Disclosure, Menu, Popover, Tab, Transition } from '@headlessui/react'
+import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
+import Link from 'next/link';
+
+const navigation = {
+  categories: [
+    {
+      id: 'women',
+      name: 'Women',
+      featured: [
+        {
+          name: 'New Arrivals',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
+          imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
+        },
+        {
+          name: 'Basic Tees',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
+          imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
+        },
+      ],
+      sections: [
+        {
+          id: 'clothing',
+          name: 'Clothing',
+          items: [
+            { name: 'Tops', href: '#' },
+            { name: 'Dresses', href: '#' },
+            { name: 'Pants', href: '#' },
+            { name: 'Denim', href: '#' },
+            { name: 'Sweaters', href: '#' },
+            { name: 'T-Shirts', href: '#' },
+            { name: 'Jackets', href: '#' },
+            { name: 'Activewear', href: '#' },
+            { name: 'Browse All', href: '#' },
+          ],
+        },
+        {
+          id: 'accessories',
+          name: 'Accessories',
+          items: [
+            { name: 'Watches', href: '#' },
+            { name: 'Wallets', href: '#' },
+            { name: 'Bags', href: '#' },
+            { name: 'Sunglasses', href: '#' },
+            { name: 'Hats', href: '#' },
+            { name: 'Belts', href: '#' },
+          ],
+        },
+        {
+          id: 'brands',
+          name: 'Brands',
+          items: [
+            { name: 'Full Nelson', href: '#' },
+            { name: 'My Way', href: '#' },
+            { name: 'Re-Arranged', href: '#' },
+            { name: 'Counterfeit', href: '#' },
+            { name: 'Significant Other', href: '#' },
+          ],
+        },
+      ],
+    },
+    {
+      id: 'men',
+      name: 'Men',
+      featured: [
+        {
+          name: 'New Arrivals',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
+          imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
+        },
+        {
+          name: 'Artwork Tees',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
+          imageAlt:
+            'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
+        },
+      ],
+      sections: [
+        {
+          id: 'clothing',
+          name: 'Clothing',
+          items: [
+            { name: 'Tops', href: '#' },
+            { name: 'Pants', href: '#' },
+            { name: 'Sweaters', href: '#' },
+            { name: 'T-Shirts', href: '#' },
+            { name: 'Jackets', href: '#' },
+            { name: 'Activewear', href: '#' },
+            { name: 'Browse All', href: '#' },
+          ],
+        },
+        {
+          id: 'accessories',
+          name: 'Accessories',
+          items: [
+            { name: 'Watches', href: '#' },
+            { name: 'Wallets', href: '#' },
+            { name: 'Bags', href: '#' },
+            { name: 'Sunglasses', href: '#' },
+            { name: 'Hats', href: '#' },
+            { name: 'Belts', href: '#' },
+          ],
+        },
+        {
+          id: 'brands',
+          name: 'Brands',
+          items: [
+            { name: 'Re-Arranged', href: '#' },
+            { name: 'Counterfeit', href: '#' },
+            { name: 'Full Nelson', href: '#' },
+            { name: 'My Way', href: '#' },
+          ],
+        },
+      ],
+    },
+  ],
+  pages: [
+    { name: 'Company', href: '#' },
+    { name: 'Stores', href: '#' },
+  ],
+}
+const sortOptions = [
+  { name: 'Most Popular', href: '#', current: true },
+  { name: 'Best Rating', href: '#', current: false },
+  { name: 'Newest', href: '#', current: false },
+  { name: 'Price: Low to High', href: '#', current: false },
+  { name: 'Price: High to Low', href: '#', current: false },
+]
+const subCategories = [
+  { name: 'Top Products', href: '#' },
+  { name: 'All gift cards', href: '#' },
+  { name: 'Ecommerce', href: '#' },
+  { name: 'Electronics', href: '#' },
+  { name: 'Entertainment', href: '#' },
+  { name: 'Food', href: '#' },
+  { name: 'Games', href: '#' },
+]
+const filters = [
+  {
+    id: 'color',
+    name: 'Color',
+    options: [
+      { value: 'white', label: 'White', checked: false },
+      { value: 'beige', label: 'Beige', checked: false },
+      { value: 'blue', label: 'Blue', checked: true },
+      { value: 'brown', label: 'Brown', checked: false },
+      { value: 'green', label: 'Green', checked: false },
+      { value: 'purple', label: 'Purple', checked: false },
+    ],
+  },
+  {
+    id: 'category',
+    name: 'Category',
+    options: [
+      { value: 'new-arrivals', label: 'New Arrivals', checked: false },
+      { value: 'sale', label: 'Sale', checked: false },
+      { value: 'travel', label: 'Travel', checked: true },
+      { value: 'organization', label: 'Organization', checked: false },
+      { value: 'accessories', label: 'Accessories', checked: false },
+    ],
+  },
+  {
+    id: 'size',
+    name: 'Size',
+    options: [
+      { value: '2l', label: '2L', checked: false },
+      { value: '6l', label: '6L', checked: false },
+      { value: '12l', label: '12L', checked: false },
+      { value: '18l', label: '18L', checked: false },
+      { value: '20l', label: '20L', checked: false },
+      { value: '40l', label: '40L', checked: true },
+    ],
+  },
+]
+const products = [
+  {
+    id: 1,
+    name: 'Mtn',
+    href: '/recharge',
+    price: '$50',
+    availability: 'White and Black',
+    imageSrc: 'https://images.africanfinancials.com/63be93ed-ng-mtn-logo-200x200.png',
+    imageAlt: 'White fabric pouch with white zipper, black zipper pull, and black elastic loop.',
+  },
+  {
+    id: 2,
+    name: 'Airtel',
+    href: '#',
+    price: '$140',
+    availability: 'Washed Black',
+    imageSrc: 'https://www.businesslist.com.ng/img/ng/e/1604481877-59-airtel-nigeria-customer-care.jpg',
+    imageAlt: 'Front of tote bag with washed black canvas body, black straps, and tan leather handles and accents.',
+  },
+  {
+    id: 3,
+    name: 'Glo',
+    href: '#',
+    price: '$140',
+    availability: 'Washed Black',
+    imageSrc: 'https://netstorage-legit.akamaized.net/images/a3d8cbb3f3a7b1d6.jpg?imwidth=720',
+    imageAlt: 'Front of tote bag with washed black canvas body, black straps, and tan leather handles and accents.',
+  },
+
+  {
+    id: 4,
+    name: '9mobile',
+    href: '#',
+    price: '$140',
+    availability: 'Washed Black',
+    imageSrc: 'https://9mobile.com.ng/wp-content/uploads/2023/02/9xtra-all-channels-600x601.png',
+    imageAlt: 'Front of tote bag with washed black canvas body, black straps, and tan leather handles and accents.',
+  },
+  // More products...
+]
+const footerNavigation = {
+  account: [
+    { name: 'Manage Account', href: '#' },
+    { name: 'Saved Items', href: '#' },
+    { name: 'Orders', href: '#' },
+    { name: 'Redeem Gift card', href: '#' },
+  ],
+  service: [
+    { name: 'Shipping & Returns', href: '#' },
+    { name: 'Warranty', href: '#' },
+    { name: 'FAQ', href: '#' },
+    { name: 'Find a store', href: '#' },
+    { name: 'Get in touch', href: '#' },
+  ],
+  company: [
+    { name: 'Who we are', href: '#' },
+    { name: 'Press', href: '#' },
+    { name: 'Careers', href: '#' },
+    { name: 'Terms & Conditions', href: '#' },
+    { name: 'Privacy', href: '#' },
+  ],
+  connect: [
+    { name: 'Instagram', href: '#' },
+    { name: 'Pinterest', href: '#' },
+    { name: 'Twitter', href: '#' },
+  ],
+}
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+<style jsx>{`
+  @media (max-width: 640px) {
+    .text-me {
+      margin-top: -200px;
+    }
+  }
+
+
+`}</style>
+
+export default function GiftCard() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
+
+  return (
+    <div>
+      
+
+      <div>
+
+        <main className="mx-auto max-w-7xl mt-20 px-4 sm:px-6 lg:px-8">
+          <div className="flex items-baseline justify-between">
+          <h1 className="text-me text-center lg:ml-[322px] sm:-mt-[200px] lg:text-2xl sm:text-[30px] sm:mr-3 font-extrabold tracking-tight text-gray-900">
+  Top up Your Phone
+</h1>
+
+            <div className="flex items-center">
+              <div className="relative inline-block text-left">
+                <div>
+                  <Link href="#" className="mr-5 group inline-flex justify-center text-sm underline font-medium text-gray-700 hover:text-gray-900">
+                    See all
+                  </Link>
+                </div>
+
+              </div>
+
+      <button
+        type="button"
+        className="relative inline-flex items-center rounded-l-md bg-white px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      >
+        <span className="sr-only">Previous</span>
+        <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+      </button>
+      <button
+        type="button"
+        className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      >
+        <span className="sr-only">Next</span>
+        <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+      </button>
+              <button
+                type="button"
+                className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
+                onClick={() => setMobileFiltersOpen(true)}
+              >
+                <span className="sr-only">Filters</span>
+                <FunnelIcon className="h-5 w-5" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+
+          <section aria-labelledby="products-heading" className="pb-24 pt-6">
+            <h2 id="products-heading" className="sr-only">
+              Products
+            </h2>
+
+            <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
+              {/* Filters */}
+              <form className="hidden lg:block">
+                <h3 className="sr-only">Categories</h3>
+                <ul  style={{ marginBottom: '-322px'}}role="list" className="space-y-4 border-gray-200 pb-6 text-1xl font-bold text-gray-900">
+                  {subCategories.map((category) => (
+                    <li className="hover:underline" key={category.name}>
+                      <a href={category.href}>{category.name}</a>
+                    </li>
+                  ))}
+                </ul>
+
+              
+              </form>
+
+              {/* Product grid */}
+              <div className="grid grid-cols-4 gap-x-6 gap-y-20 sm:grid-cols-4 lg:col-span-3 lg:gap-x-8">
+                {products.map((product) => (
+                  <Link key={product.id} href={product.href} className="group text-sm">
+                    <div className="aspect-w-3 w-full overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                      <img
+                        src={product.imageSrc}
+                        alt={product.imageAlt}
+                        className="h-full object-cover object-center"
+                      />
+                    </div>
+                  </Link>
+                ))}
+              </div>
+
+            
+            </div>
+          </section>
+        </main>
+      </div>
+    </div>
+  )
+}
