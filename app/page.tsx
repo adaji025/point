733f050cc56b111db040c56492b724d42810a888
Anibe <samanibe@yahoo.com>
2023-06-01 16:28:30 +0100

    added components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import TopNavbar from "@/components/Navbar/TopNavbar";
+import GiftCard from "@/components/Home/GiftCard";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
       <main className="grow">
-        <TopNavbar />
         <Navbar />
+        <GiftCard/>
+        <Footer/>
       </main>
     </div>
 
