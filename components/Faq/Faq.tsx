30f6951e2a4989bb919dcd9d0d80cafce809ecb4
Anibe <samanibe@yahoo.com>
2023-06-05 13:29:50 +0100

    fix components

---
+++ components/Faq/Faq.tsx
@@ -0,0 +1,55 @@
+"use client"
+import { Disclosure } from '@headlessui/react'
+import { MinusSmallIcon, PlusSmallIcon } from '@heroicons/react/24/outline'
+
+const faqs = [
+  {
+    question: "Can I buy gift cards with Bitcoin or Crypto?",
+    answer:
+      "Yes, you can buy gift cards with Bitcoin or Crypto. You can cash out your crypto to purchase over 5,000+ gift cards from a variety of retailers, including Amazon, Apple, Walmart, Steam, Google Play etc.",
+  },
+
+  {
+    question: "What cryptocurrencies can I use to shop gift cards?",
+    answer:
+      "With Points you can purchase gift cards using a variety of cryptocurrencies including Bitcoin, Lightning, Ethereum, USDC, USDT, Binance Pay, Litecoin, Dogecoin or Dash.",
+  },
+  // More questions...
+]
+
+export default function Faq() {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
+        <div className="mx-auto max-w-4xl divide-y divide-gray-900/10">
+          <h2 className="text-4xl font-bold leading-10 tracking-tight text-gray-900">Frequently asked questions</h2>
+          <dl className="mt-10 space-y-6 divide-y divide-gray-900/10">
+            {faqs.map((faq) => (
+              <Disclosure as="div" key={faq.question} className="pt-6">
+                {({ open }) => (
+                  <>
+                    <dt>
+                      <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-900">
+                        <span className="text-base font-semibold leading-7">{faq.question}</span>
+                        <span className="ml-6 flex h-7 items-center">
+                          {open ? (
+                            <MinusSmallIcon className="h-6 w-6" aria-hidden="true" />
+                          ) : (
+                            <PlusSmallIcon className="h-6 w-6" aria-hidden="true" />
+                          )}
+                        </span>
+                      </Disclosure.Button>
+                    </dt>
+                    <Disclosure.Panel as="dd" className="mt-2 pr-12">
+                      <p className="text-base leading-7 text-gray-600">{faq.answer}</p>
+                    </Disclosure.Panel>
+                  </>
+                )}
+              </Disclosure>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  )
+}
