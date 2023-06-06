445bb1b6abf3f512bf0f605a62f4f97ffb89b4e7
Adaji Mukhtar <alfadaji@gmail.com>
2023-06-06 22:06:47 +0100

    home page ready

--- components/Home/Collection.tsx
+++ components/Home/Collection.tsx
@@ -11,7 +11,7 @@ type CardProps = {
 
 const Card = ({ item }: CardProps) => {
   return (
-    <div className="max-w-[400px] h-[300px] xl:h-[450px] relative cursor-pointer hover:scale-105 transition-all duration-300">
+    <div className="max-w-[400px] h-[300px] xl:h-[450px] relative cursor-pointer hover:scale-105 transition-all duration-300 mx-auto">
       <img src={item.image} className="w-full h-full object-cover" alt="" />
       <div className="font-bold text-2xl text-white text-center bg-black/50 p-4 absolute top-10 w-full">
         {item.title}
@@ -46,7 +46,7 @@ const Collection = () => {
           Explore More Collections
         </h2>
       </Link>
-      <div className="mt-5 flex gap-10 justify-between">
+      <div className="mt-5 flex flex-col sm:flex-row justify-center gap-10 sm:justify-between">
         {collectionData.map((item, idx) => (
           <Card key={idx} {...{ item }} />
         ))}
