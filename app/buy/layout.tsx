bc97486daeff0ff84454ae9c759bf43b8c1065ca
Adaji Mukhtar <alfadaji@gmail.com>
2023-06-07 23:17:42 +0100

    started the entertainment page

---
+++ app/buy/layout.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import React from "react";
+import Sidebar from "./Sidebar";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+  const [active, setActive] = React.useState("Top Products");
+  const [isOpen, setIsOpen] = React.useState(true);
+  return (
+    <div className="flex max-w-[1400px] mx-auto px-4 mt-10">
+      <div className="hidden mobile:block mt-3">
+        <Sidebar {...{ active, setActive }} />
+      </div>
+      <main className="w-full mobile:w-[calc(100vw-250px)] lg:w-[calc(100vw-300px)]">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
