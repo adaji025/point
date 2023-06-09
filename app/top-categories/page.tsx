82e94c346a5c9e603f17c4a6e02f649547252ea8
Anibe <samanibe@yahoo.com>
2023-06-09 11:53:28 +0100

    checkout

--- app/top-categories/page.tsx
+++ app/top-categories/page.tsx
@@ -1,11 +1,25 @@
-import GiftCard from '@/components/Home/GiftCard'
 import React from 'react'
+import GiftCard from '@/components/Home/GiftCard'
+import Navbar from '@/components/Navbar/Navbar'
+import Footer from '@/components/Footer/Footer'
+
+export const metadata = {
+  title: "Points - Top Catigories",
+};
+
 
 const TopCategory = () => {
   return (
-    <div>
-      <GiftCard />
+
+    <>
+    <div className="flex flex-col min-h-screen overflow-hidden">
+      <main className="grow">
+        <Navbar/>
+        <GiftCard />
+        <Footer/>
+      </main>
     </div>
+  </>
   )
 }
 
