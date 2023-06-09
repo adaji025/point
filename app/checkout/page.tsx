8a36a9922c3258ab30bcd59f72509eed0eb9798f
Anibe <samanibe@yahoo.com>
2023-06-09 11:52:38 +0100

    checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,19 +1,23 @@
-import Head from "next/head";
-import Navbar from "@/components/Navbar/Navbar";
-import Footer from "@/components/Footer/Footer";
+import Checkout from "@/components/Checkout/Checkout";
+import CheckoutNavbar from "@/components/Navbar/CheckoutNavbar";
+
+
+export const metadata = {
+  title: "Shopping Cart - Points",
+};
+
 
 export default function CheckoutPage() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-      <main className="grow">
-      <Head>
-        <title> Shopping Cart - Points</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-        <Navbar />
-        <Footer/>
-      </main>
-    </div>
+    <>
+      <div className="flex flex-col min-h-screen overflow-hidden">
+        <main className="grow">
+          <CheckoutNavbar/>
+          <Checkout />
+        </main>
+      </div>
+    </>
+
 
   )
 }
