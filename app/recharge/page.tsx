b90f6e809147692ac3f2f5cf78b9bbf4ba0e351a
Anibe <samanibe@yahoo.com>
2023-06-09 11:52:54 +0100

    checkout

--- app/recharge/page.tsx
+++ app/recharge/page.tsx
@@ -1,17 +1,21 @@
-import Head from "next/head";
+
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 import ProductDetails from "@/components/Product/ProductDetails";
+
+export const metadata = {
+  title: "Recharge - Topup on Points",
+};
+
+
 export default function RechargePage() {
   return (
     <>
-      <Head>
-        <title> Recharge - Topup on Points</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className="flex flex-col min-h-screen overflow-hidden">
         <main className="grow">
+          <Navbar/>
           <ProductDetails />
+          <Footer/>
         </main>
       </div>
     </>
