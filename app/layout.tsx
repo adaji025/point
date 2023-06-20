a5c85b0bd5bb034f6e21762ec38565b1ae67626c
Adaji Mukhtar <alfadaji@gmail.com>
2023-06-20 18:27:14 +0100

    made amends to the code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,6 @@
-"use client";
-
-
-import NextTopLoader from 'nextjs-toploader';
-import Navbar from "@/components/Navbar/Navbar";
-import { Inter } from "next/font/google";
-import Footer from "@/components/Footer/Footer";
-const inter = Inter({ subsets: ["latin"] });
 import "../styles/globals.css";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Points - Buy gift cards, pay utility bills with crypto",
@@ -19,15 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <html lang="en">
-        <body className={`bg-black/5 ${inter.className}`}>
-        <NextTopLoader color="#2A7671" height={5} />
-          <Navbar />
-          {children}
-          <Footer />
-        </body>
-      </html>
-    </>
+    <html lang="en">
+      <body className={`bg-black/5 ${inter.className}`}>    
+        {children}    
+      </body>
+    </html>
   );
 }
