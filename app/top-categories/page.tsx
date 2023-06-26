e592ee54d3b410338cc1a7218dc76794551c3590
Adaji Mukhtar <alfadaji@gmail.com>
2023-06-26 10:26:22 +0100

    removed redundant import

--- app/top-categories/page.tsx
+++ app/top-categories/page.tsx
@@ -1,19 +1,8 @@
 "use client"
 
 import React from "react";
-import Link from "next/link";
-import { ChevronLeftIcon, ChevronRightIcon, StarIcon } from "@heroicons/react/24/outline";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation } from "swiper";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
 import PhoneTopup from "@/components/TopCategories/PhoneTopup";
 import UtilityBills from "@/components/TopCategories/UtilityBills";
-
-SwiperCore.use([Navigation]);
 
 export const metadata = {
   title: "Points - Top Catigories",
@@ -22,10 +11,6 @@ export const metadata = {
 
 
 const TopCategory = () => {
-  
-
- 
-
   return (
     <>
       <PhoneTopup />
