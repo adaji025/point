ea0cd94cf79d576fca08383a69cb382529fa1c59
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2023-09-05 10:40:56 +0100

    restructure api log and service function and fetched single product

--- api/api.ts
+++ api/api.ts
@@ -1,7 +1,9 @@
-const api = process.env.NEXT_PUBLIC_REACT_APP_API_URL
+const api = process.env.NEXT_PUBLIC_REACT_APP_API_URL;
 
 export const APIS = {
-    PRODUCTS: {
-        GET_PRODUCTS: (country_code: string,) =>`${api}/products?category=airtime&country=GH&limit=5`
-    }
-}
+  PRODUCTS: {
+    GET_PRODUCTS: (category: string, country_code: string, limit: number) =>
+      `${api}/products?category=${category}&country=${country_code}&limit=${limit}`,
+    GET_SINGLE_PRODUCT: (id: string) => `${api}/products/${id}`,
+  },
+};
