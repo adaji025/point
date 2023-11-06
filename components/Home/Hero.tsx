2aca58860799de00164aed44dfdb55c513b979ed
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2023-11-06 22:06:57 +0100

    made achanges the hero section

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,12 +1,11 @@
-
-"use client"
+"use client";
 import React from "react";
 import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
 import styles from "../../styles/Home.module.css";
 import { useRouter } from "next/navigation";
 
 const Hero = () => {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <div className={`min-h-[70vh] flex items-center ${styles.hero}`}>
       <div className="max-w-[1400px] w-full mx-auto text-white px-6 lg:px-16">
@@ -15,18 +14,14 @@ const Hero = () => {
             Search. Shop. Pay.
           </h2>
           <p className="text-[18px] md:text-[22px]">
-          Experience a revolutionary way to shop with multiple options.
+            Play Games and do more with your Points in the real World!
           </p>
-          <div className="mt-5 flex gap-5 items-center">
-            <button className="border flex items-center justify-center gap-2 w-[150px] py-2 px-3 rounded-full hover:scale-105 transition-all duration-200"
-            onClick={() => router.push("/top-categories")}>
-              <span className="whitespace-nowrap text-sm">Phone Refill</span>
-              <ArrowLongRightIcon className="h-5 w-5" />
+          <div className="mt-7 flex gap-5 items-center">
+            <button className="text-white px-6 py-3 rounded-full bg-black text-sm hover:scale-105 duration-500 transition-all">
+              App Store
             </button>
-            <button className="border flex items-center justify-center gap-2 w-[150px] py-2 px-3 rounded-full hover:scale-105 transition-all duration-200"
-            onClick={() => router.push("/top-categories")}>
-              <span className="whitespace-nowrap text-sm">Gift Cards</span>
-              <ArrowLongRightIcon className="h-5 w-5" />
+            <button className="text-white px-6 py-3 rounded-full bg-black text-sm hover:scale-105 duration-500 transition-all">
+              Google Play
             </button>
           </div>
         </div>
